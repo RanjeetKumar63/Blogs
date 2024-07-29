f08824940f9e1015aec2e50f4be550c6714fef3c
Ranjeet Kumar <143226519+RanjeetKumar63@users.noreply.github.com>
2024-07-30 00:10:49 +0530

    blogs

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="border text-center py-3 shadow-md w-full">
+      <header>
+        <h1 className="text-2xl font-extrabold">exotic_coder_Blogs</h1>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
